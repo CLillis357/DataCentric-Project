bd8db22cadc75b9769014a82204121980168563d
Conor Lillis <G00386269@atu.ie>
2024-12-29 21:59:20 +0000

    Home page created and set up to add functionality

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-var express = require("express");
+var express = require('express');
+var bodyParser = require('body-parser');
+const { render } = require('express/lib/response');
+//var mySQLDAO = require('./mySQLDAO');
+//var mongoDAO = require('./mongoDAO');
 var app = express();
+const port = 3004;
 
 
 app.listen(3004, () => {
-  console.log("running on port 3004");
+console.log("running on port 3004");
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/Home.html");
 });
-console.log("Here");
+});
